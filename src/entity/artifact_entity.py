58cceb72e3fe90ef47c77fa97cf6eaf332571bfd
Komal Mandal <komalmandal1907@gmail.com>
2025-12-29 11:59:35 +0530

    model evaluation and model pusher on aws s3 is completed

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -34,5 +34,19 @@ class ModelTrainerArtifact:
          self.f1_score = f1_score
 
 
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
 
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str
 
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str
